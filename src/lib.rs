6173f1e6d2f64ced2b946ab78ad42fb6212f8c36
Lassi Raatikainen <raatikainenlassi@gmail.com>
2025-03-07 11:35:13 +0200

    wip: start iteration through self.buffer

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,23 @@ use pyo3::prelude::*;
 #[pyclass]
 pub struct FixParser {
     buffer: Vec<u8>,
+    equal_byte: u8,
+    soh_byte: u8,
+    end_tag: u8,
+    start_tag: u8,
 }
 
 #[pymethods]
 impl FixParser {
     #[new]
-    pub fn new() -> Self {
-        FixParser { buffer: Vec::new() }
+    pub fn new(equal_byte: u8, soh_byte: u8, end_tag: u8, start_tag: u8) -> Self {
+        FixParser {
+            buffer: Vec::new(),
+            equal_byte,
+            soh_byte,
+            end_tag,
+            start_tag,
+        }
     }
 
     pub fn add_buffer(&mut self, data: &[u8]) {
@@ -17,12 +27,28 @@ impl FixParser {
     }
 
     pub fn get_message(&mut self) -> Option<Vec<u8>> {
-        if let Some(pos) = self.buffer.windows(1).position(|w| w == &[b'\n']) {
-            let message = self.buffer.drain(..=pos).collect();
-            Some(message)
-        } else {
-            None
+        let mut pos: usize = 0;
+        let buffer_length: usize = self.buffer.len();
+        let mut in_tag: bool = true;
+
+        while pos < buffer_length {
+            // Assume we have TAG=VALUE.
+            // We are currently in TAG
+            if in_tag {
+                // Find next position that contains b"="
+                let equals_pos: usize =
+                    match self.buffer.windows(1).position(|w| w == &[self.equal_byte]) {
+                        // Found b"="
+                        Some(b) => b,
+                        // Did not find b"=", so we can early exit.
+                        None => return None,
+                    };
+            } else {
+            }
         }
+
+        let message: Vec<u8> = self.buffer.drain(..=pos).collect();
+        Some(message)
     }
 }
 
