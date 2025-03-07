f8c787618ef6a6591196ef9850edf46d786c5b18
Lassi Raatikainen <raatikainenlassi@gmail.com>
2025-03-07 10:35:04 +0200

    feat: add dummy version of FixParser class

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,31 @@
 use pyo3::prelude::*;
 
+#[pyclass]
+pub struct FixParser {
+    buffer: Vec<u8>,
+}
+
+#[pymethods]
+impl FixParser {
+    #[new]
+    pub fn new() -> Self {
+        FixParser { buffer: Vec::new() }
+    }
+
+    pub fn add_buffer(&mut self, data: &[u8]) {
+        self.buffer.extend_from_slice(data);
+    }
+
+    pub fn get_message(&mut self) -> Option<Vec<u8>> {
+        if let Some(pos) = self.buffer.windows(1).position(|w| w == &[b'\n']) {
+            let message = self.buffer.drain(..=pos).collect();
+            Some(message)
+        } else {
+            None
+        }
+    }
+}
+
 /// Formats the sum of two numbers as string.
 #[pyfunction]
 fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
@@ -10,5 +36,6 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 #[pymodule]
 fn farser(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_class::<FixParser>()?;
     Ok(())
 }
